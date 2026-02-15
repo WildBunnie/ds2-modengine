49486046e5106c2b9e3dd7471cd26ff4895e1c0f
WildBunnie <wildbunnie2003@gmail.com>
2026-02-15 14:11:49 +0000

    add title:/ override again
    params were not loading properly without it

--- src/modengine.cpp
+++ src/modengine.cpp
@@ -80,9 +80,9 @@ uintptr_t ds2_detour_virtual_to_archive_path(uintptr_t p1, DLString* path)
     // ignore capacity above 7 to not have to deal with SSO bullshit
     if (path && path->length > 0 && path->string) {
         // NOTE: i have no idea which things i actually need to override
-        // overriding gamedata:/ seems to be enough for now
+        // overriding gamedata:/ and title:/ seems to be enough for now
         int prefix_len = -1;
-        // if (wstring_starts_with(path->string, L"title:/")) prefix_len = 7;
+        if (wstring_starts_with(path->string, L"title:/")) prefix_len = 7;
         if (wstring_starts_with(path->string, L"gamedata:/")) prefix_len = 10;
 
         if (prefix_len != -1) {
